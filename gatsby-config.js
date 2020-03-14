9a0baaa9fe47ae7e9b55d7218ac3a0e50a0f1b5a
Brandon Noad <Brandon.Noad@gmail.com>
2020-03-14 11:08:42 -0300

    Add gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,8 @@ module.exports = {
         {
             resolve: `gatsby-plugin-create-client-paths`,
             options: { prefixes: [`/app/*`] }
-        }
+        },
+        `gatsby-plugin-netlify`
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
